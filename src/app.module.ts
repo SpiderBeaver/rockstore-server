dcd60b91c6dc12e3c1e09e0c884bfeddc084ac1d
SpiderBeaver <spdrbeaver@gmail.com>
2021-06-23 16:30:51 +0300

    Install typeorm.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Product } from './products/product.entity';
 import { ProductsController } from './products/products.controller';
 
 @Module({
-  imports: [],
+  // TODO: Move typeorm confing into .env file.
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'password',
+      database: 'rockstore',
+      entities: [Product],
+      synchronize: true,
+    }),
+    TypeOrmModule.forFeature([Product]),
+  ],
   controllers: [AppController, ProductsController],
   providers: [AppService],
 })
--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -5,27 +5,31 @@ import {
   HttpStatus,
   Param,
 } from '@nestjs/common';
-import { Product } from './product.interface';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Product } from './product.entity';
 
 @Controller('products')
 export class ProductsController {
-  private products: Product[] = [
-    { id: 0, name: 'Product 1' },
-    { id: 1, name: 'Product 2' },
-  ];
+  constructor(
+    @InjectRepository(Product)
+    private productsRepository: Repository<Product>,
+  ) {}
 
   @Get()
-  findAll() {
-    return this.products;
+  async findAll() {
+    const products = await this.productsRepository.find();
+    return products;
   }
 
   @Get(':id')
-  findById(@Param('id') idString: string) {
+  async findById(@Param('id') idString: string) {
     const id = parseInt(idString);
-    const product = this.products.find((p) => p.id === id);
+    const product = await this.productsRepository.findOne(id);
     if (!product) {
       throw new HttpException('Product not found', HttpStatus.NOT_FOUND);
     }
+
     return product;
   }
 }
